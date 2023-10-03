1ab121f10e98b43212a7bde41754164930b39eb4
dharasoni16 <99190748+dharasoni16@users.noreply.github.com>
2023-10-03 13:53:34 +0200

    fix:test-auth-server.html for 404 error

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -15,45 +15,10 @@ const oAuth2Client = new google.auth.OAuth2(
 );
 module.exports.getAuthURL = async () => {
   /* Scopes array is passed to the `scope` option. */
-  const authURL = oAuth2Client.generateAuthUrl({
+  const authUrl = oAuth2Client.generateAuthUrl({
     access_type: "offline",
     scope: SCOPES,
   });
-
-  module.exports.getAccessToken = async (event) => {
-    // Decode authorization code extracted from the URL query
-    const code = decodeURIComponent(`${event.pathParameters.code}`);
-
-    return new Promise((resolve, reject) => {
-      /* Exchange authorization code for access token with a "callback" after the exchange,
-      The callback in this case is an arrow function with the results a parameters: "error" and "response" */
-
-      oAuth2Client.getToken(code, (error, response) => {
-        if (error) {
-          return reject('Error retrieving access token', error);
-        }
-        return resolve(response);
-      });
-    })
-      .then((results) => {
-        // Respond with OAuth token
-        return {
-          statusCode: 200,
-          headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Credentials': true,
-          },
-          body: JSON.stringify(results),
-        };
-      })
-      .catch((error) => {
-        // Handle error
-        return {
-          statusCode: 500,
-          body: JSON.stringify(error),
-        };
-      });
-  };
   return {
     statusCode: 200,
     headers: {
@@ -62,10 +27,46 @@ module.exports.getAuthURL = async () => {
     },
     body: JSON.stringify(
       {
-        authURL,
+        authUrl,
       }),
   };
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
+
+module.exports.getAccessToken = async (event) => {
+  // Decode authorization code extracted from the URL query
+  const code = decodeURIComponent(`${event.pathParameters.code}`);
+
+  return new Promise((resolve, reject) => {
+    /* Exchange authorization code for access token with a "callback" after the exchange,
+    The callback in this case is an arrow function with the results a parameters: "error" and "response" */
+
+    oAuth2Client.getToken(code, (error, response) => {
+      if (error) {
+        return reject('Error retrieving access token', error);
+      }
+      return resolve(response);
+    });
+  })
+    .then((results) => {
+      // Respond with OAuth token
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+        },
+        body: JSON.stringify(results),
+      };
+    })
+    .catch((error) => {
+      // Handle error
+      return {
+        statusCode: 500,
+        body: JSON.stringify(error),
+      };
+    });
+};
+
