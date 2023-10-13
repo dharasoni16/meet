d2b8f2b6603879b028f40b32ddc104a1cc185fbd
dharasoni16 <99190748+dharasoni16@users.noreply.github.com>
2023-10-13 17:10:27 +0200

    Add:info and error alerts and some css

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const CitySearch = ({ allLocations, setCurrentCity }) => {
+const CitySearch = ({ allLocations, setCurrentCity, setInfoAlert }) => {
     const [showSuggestions, setShowSuggestions] = useState(false);
     const [query, setQuery] = useState("");
     const [suggestions, setSuggestions] = useState([]);
@@ -16,6 +16,14 @@ const CitySearch = ({ allLocations, setCurrentCity }) => {
         }) : [];
         setQuery(value);
         setSuggestions(filteredLocations);
+
+        let infoText;
+        if (filteredLocations.length === 0) {
+            infoText = "We can not find the city you are looking for.Please try another city";
+        } else {
+            infoText = "";
+        }
+        setInfoAlert(infoText);
     };
 
     const handleItemClicked = (event) => {
@@ -23,10 +31,12 @@ const CitySearch = ({ allLocations, setCurrentCity }) => {
         setQuery(value);
         setShowSuggestions(false);// to hide the list
         setCurrentCity(value);
+        setInfoAlert("");
     };
     return (
         <div id="city-search">
-            <input type="text" className="city" placeholder="Search for a city" value={query} onFocus={() => setShowSuggestions(true)} onChange={handleInputChanged} />
+            <label htmlFor="event-location">Enter the location</label>
+            <input type="text" className="city" placeholder="Search for a city" id="event-location" value={query} onFocus={() => setShowSuggestions(true)} onChange={handleInputChanged} />
             {showSuggestions ? <ul className="suggestions">{suggestions.map((suggestion) => {
                 return <li onClick={handleItemClicked} key={suggestion}>{suggestion}</li>
             })}<li key='See all cities' onClick={handleItemClicked}><b>See all cities</b></li></ul> : null}
--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -1,9 +1,19 @@
-const NumberOfEvents = ({ setCurrentNOE }) => {
+const NumberOfEvents = ({ setCurrentNOE, setErrorAlert }) => {
     const handleInputChanged = (event) => {
         const value = event.target.value;
-        setCurrentNOE(value);
-    }
+        let errorText;
+        if (isNaN(value)) {
+            errorText = "Entered value is not a number";
+            setErrorAlert(errorText);
+        } else if (value <= 0) {
+            errorText = "Please enter the value greater than 0";
+            setErrorAlert(errorText);
+        } else {
+            setErrorAlert("");
+            setCurrentNOE(value);
 
+        }
+    }
     return (
         <div id="number-of-events">
             <label htmlFor='number-of-events-input'>Number of Events: </label>
