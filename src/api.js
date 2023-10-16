59a3757a5d9244e156edcfba172768e83c38c244
dharasoni16 <99190748+dharasoni16@users.noreply.github.com>
2023-10-16 15:48:49 +0200

    Add:offline capability

--- src/api.js
+++ src/api.js
@@ -36,7 +36,6 @@ export const getEvents = async () => {
     }
     if (!navigator.onLine) {
         const events = localStorage.getItem("lastEvents");
-        NProgress.done();
         return events ? JSON.parse(events) : [];
     }
     const token = await getAccessToken();
@@ -46,7 +45,6 @@ export const getEvents = async () => {
         const response = await fetch(url);
         const result = await response.json();
         if (result) {
-            NProgress.done();
             localStorage.setItem("lastEvents", JSON.stringify(result.events));
             return result.events;
         } else {
