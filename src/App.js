4f8adfef3f748550a7f9391ff849f5aebaca94a4
dharasoni16 <99190748+dharasoni16@users.noreply.github.com>
2023-10-17 16:28:36 +0200

    Add:ScatterChart from recharts

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NumberOfEvents from './components/NumberOfEvents';
 import { useEffect, useState } from 'react';
 import { extractLocations, getEvents } from './api';
 import { InfoAlert, ErrorAlert, WarningAlert } from './components/Alert';
+import CityEventsChart from './components/CityEventsChart';
 import './App.css';
 
 const App = () => {
@@ -34,11 +35,12 @@ const App = () => {
 
   return (
     <div className="App">
+      <h1>Meet App</h1>
       <div className="alerts-container">{infoAlert.length ? <InfoAlert text={infoAlert} /> : null}
         {errorAlert.length ? <ErrorAlert text={errorAlert} /> : null}{warningAlert.length ? <WarningAlert text={warningAlert} /> : null}</div>
-      <h1>Meet Event App</h1>
       <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} setInfoAlert={setInfoAlert} />
       <NumberOfEvents setCurrentNOE={setCurrentNOE} setErrorAlert={setErrorAlert} />
+      <CityEventsChart allLocations={allLocations} events={events} />
       <EventList events={events} />
     </div>
   );
